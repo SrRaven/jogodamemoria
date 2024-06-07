4f7595662686e9e78b5c5863179ccc8d16a27bc9
Douglas <87214871+SrRaven@users.noreply.github.com>
2024-06-07 16:58:11 -0300

    Novas funções
    mudanças nas funções e adição de novos elementos como "tela inicial"

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,12 @@ function shuffleArray(array) {
     return array;
 }
 
+
 const shuffledCards = shuffleArray(cards);
 console.log(shuffledCards); // Imprimindo o array embaralhado
 
 const gameBoard = document.querySelector('.game-board');
+gameBoard.style.display = 'grid';
 
 shuffledCards.forEach((cardNumber) => {
     const card = document.createElement('div');
@@ -43,4 +45,39 @@ setTimeout(() => {
     flippedCard = null;
 }, 1000);
 }
+}
+const instructionsScreen = document.querySelector('.instructions-screen');
+const startGameBtn = document.querySelector('.start-game-btn');
+
+startGameBtn.addEventListener('click', () => {
+    instructionsScreen.style.visibility = 'hidden';
+});
+
+// Para mostrar a tela de instruções ao carregar a página
+window.addEventListener('load', () => {
+    instructionsScreen.style.visibility = 'visible';
+});
+
+const cartas = Array.from(document.querySelectorAll('.card'));
+function embaralharArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+const botaoComecarJogo = document.querySelector('.start-game-btn');
+
+botaoComecarJogo.addEventListener('click', () => {
+    embaralharCartas();
+    telaDeInstrucoes.style.visibility = 'hidden';
+});
+
+function embaralharCartas() {
+    const cartasEmbaralhadas = embaralharArray(cartas);
+    const tabuleiroDoJogo = document.querySelector('.game-board');
+    tabuleiroDoJogo.innerHTML = '';
+    cartasEmbaralhadas.forEach(carta => {
+        tabuleiroDoJogo.appendChild(carta);
+    });
 }

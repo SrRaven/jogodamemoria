9687fa4b53a26451c634a2f326a0bf7d6bc433af
Douglas <87214871+SrRaven@users.noreply.github.com>
2024-06-07 19:44:31 -0300

    Parcialmente funcional

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-const cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+const allImages = [
+    'img/image1.jpg',
+    'img/image2.jpg',
+    'img/image3.jpg',
+    'img/image4.jpg',
+    'img/image5.jpg',
+    'img/image6.jpg',
+    'img/image7.jpg',
+    'img/image8.jpg',
+    'img/image9.jpg',
+    'img/image10.jpg',
+    'img/image11.jpg',
+    'img/image12.jpg',
+    'img/image13.jpg',
+    'img/image14.jpg',
+    'img/image15.jpg',
+    'img/image16.jpg'
+];
+
+function getRandomImages(images, num) {
+    const shuffled = images.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, num);
+}
+
+const selectedImages = getRandomImages(allImages, 8);
+const cards = selectedImages.flatMap(img => [{ id: img, img }, { id: img, img }]);
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -14,16 +39,28 @@ console.log(shuffledCards); // Imprimindo o array embaralhado
 const gameBoard = document.querySelector('.game-board');
 gameBoard.style.display = 'grid';
 
-shuffledCards.forEach((cardNumber) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.cardNumber = cardNumber;
-    gameBoard.appendChild(card);
+shuffledCards.forEach((card) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.dataset.cardId = card.id;
+
+    const frontFace = document.createElement('div');
+    frontFace.classList.add('front-face');
+    frontFace.style.backgroundImage = `url(${card.img})`;
+
+    const backFace = document.createElement('div');
+    backFace.classList.add('back-face');
+
+    cardElement.appendChild(frontFace);
+    cardElement.appendChild(backFace);
+    gameBoard.appendChild(cardElement);
 });
 
 let flippedCard = null;
+let lockBoard = false;
 
 function flipCard() {
+    if (lockBoard) return;
     if (this === flippedCard) return;
 
     this.classList.toggle('flipped');
@@ -33,15 +70,17 @@ function flipCard() {
         return;
     }
 
-    if (flippedCard.dataset.cardNumber === this.dataset.cardNumber) {
+    if (flippedCard.dataset.cardId === this.dataset.cardId) {
         // Cartas correspondentes, não faça nada
         flippedCard = null;
     } else {
         // Cartas não correspondentes, volte-as
+        lockBoard = true;
         setTimeout(() => {
             flippedCard.classList.remove('flipped');
             this.classList.remove('flipped');
             flippedCard = null;
+            lockBoard = false;
         }, 1000);
     }
 }
